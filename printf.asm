f341ab96c263efff6be4c0e76ca3427de32f0212
aleksplast <aleksplast@gmail.com>
2023-03-19 18:35:35 +0300

    all should be working, lea remain

--- printf.asm
+++ printf.asm
@@ -3,7 +3,16 @@ section .text
 global _start
 
 _start:         mov rdi, Msg
-                mov rsi, 25000
+                mov rsi, Char
+                mov rdx, Number
+                mov rcx, Number
+                mov r8, Number
+                mov r9, ASS
+                push Number
+                push Char
+                push ASS
+                push rbp
+                mov rbp, rsp
                 call printf
 
                 mov rax, 0x3c
@@ -11,9 +20,12 @@ _start:         mov rdi, Msg
                 syscall
 
 printf:         push rsi
+
                 mov rsi, rdi
                 call StrLen
+                pop rsi
                 mov r12, rax
+                call Parameters
                 xor r11, r11
                 xor r10, r10
 
@@ -23,7 +35,6 @@ printf:         push rsi
                 cmp byte rdi[r10], '%'
                 jne .usual
 
-                pop rax
                 inc r10
                 call SpecificatorHandler
                 jmp .next
@@ -118,16 +129,107 @@ StrLen:	    push rsi
 ;------------------------------------------------
 
 SpecificatorHandler:
+            pop r15
             call PrintExtraBuff
-            push rax
+            cmp byte rdi[r10], '%'
+            je .PercentPrt
             xor rbx, rbx
             mov byte bl, rdi[r10]
             sub bl, 'b'
             mov rax, [JmpTable + 8 * rbx]
             jmp rax
 
-SpecOver:   inc r10
+.PercentPrt mov byte ExtraBuf[r11], '%'
+            inc r11
 
+SpecOver:   inc r10
+            push r15
+
+            ret
+
+;------------------------------------------------
+; Pushes parameters into stack
+;------------------------------------------------
+; Entry:    R12 = strlen
+;           RDI = str ptr
+; Exit:		all parameters in stack
+; Expects:	none
+; Destroys: RAX, RBX, R14
+;------------------------------------------------
+Parameters  pop r15
+            xor rax, rax
+            xor r14, r14
+
+.Next       cmp r14, r12
+            je .NumPar
+            cmp byte rdi[r14], '%'
+            je .Prbbly
+            inc r14
+            jmp .Next
+
+.Prbbly     cmp byte rdi[r14 + 1], '%'
+            jne .Sure
+            add r14, 2
+            jmp .Next
+
+.Sure       inc rax
+            add r14, 2
+            jmp .Next
+
+.NumPar     cmp rax, 5
+            ja .Stac
+            je .5par
+            cmp rax, 4
+            je .4par
+            cmp rax, 3
+            je .3par
+            cmp rax, 2
+            je .2par
+            cmp rax, 1
+            je .1par
+            jmp .Done
+
+.Stac       xor rbx, rbx
+            sub rax, 5
+.Next2      cmp rbx, rax
+            je .Regs
+            mov r14, rbp[8 + rbx * 8]
+            push r14
+            inc rbx
+            jmp .Next2
+.Regs       push r9
+            push r8
+            push rcx
+            push rdx
+            push rsi
+            jmp .Done
+
+.5par       push r9
+            push r8
+            push rcx
+            push rdx
+            push rsi
+            jmp .Done
+
+.4par       push r8
+            push rcx
+            push rdx
+            push rsi
+            jmp .Done
+
+.3par       push rcx
+            push rdx
+            push rsi
+            jmp .Done
+
+.2par       push rdx
+            push rsi
+            jmp .Done
+
+.1par       push rsi
+            jmp .Done
+
+.Done       push r15
             ret
 
 ;---------------------------------------------------------
@@ -158,37 +260,27 @@ Sspecif:    pop rax
 ;---------------------------------------------------------
 
 Ospecif:    pop rax
+            xor r13, r13
             push rsi
             push rdx
             push rax
 			xor rdx, rdx
 
 .Next1:		call CheckExtraBuf
-            cmp rdx, 8d
+            cmp rax, 0
 			je .Done1
 
 			mov rsi, rax
-			and rsi, 80000000
-			shr rsi, 12
-
-			cmp rsi, 0Ah
-			jae .PrtSym
+			and rsi, 111b
 
 			add rsi, 30h
-			mov ExtraBuf[r11], rsi
-            inc r11
-			jmp .None
+			mov DexBuf[r13], rsi
+            inc r13
 
-.PrtSym:	sub rsi, 0Ah
-			add rsi, 41h
-			mov ExtraBuf[r11], rsi
-            inc r11
-
-.None       inc rdx
-            shl rax, 4
+            shr rax, 3
 			jmp .Next1
 
-.Done1:
+.Done1:     call PrintDex
             pop rax
             pop rdx
             pop rsi
@@ -247,8 +339,7 @@ Dspecif:    pop rax
 			xor ah, ah
 			jmp .Less
 
-.Done3:		mov rsi, DexBuf
-            call PrintDex
+.Done3:	    call PrintDex
             pop rsi
             pop rdx
             pop rcx
@@ -263,7 +354,6 @@ Dspecif:    pop rax
 ;------------------------------------------------
 ; Entry:    R11 = extra buf tail
 ;           R13 = lenght of number
-;           RSI = DexBuffer ptr
 ; Exit:		R11 = tail of extra buf
 ; Expects:	none
 ; Destroys: r12
@@ -273,7 +363,7 @@ PrintDex:   push rax
 .Next       cmp r13, 0
             je .Done
             call CheckExtraBuf
-            mov rax, rsi[r13 - 1]
+            mov rax, DexBuf[r13 - 1]
             mov ExtraBuf[r11], rax
             dec r13
             inc r11
@@ -288,23 +378,25 @@ Bspecif:    pop rax
             push rax
             push rdx
             xor dx, dx
+            xor r13, r13
 
 .Next2:		call CheckExtraBuf
-            cmp dx, 16d
+            cmp rax, 0
 			je .Done2
 
-			shl ax, 1
+			shr rax, 1
 			jc .PrtZ
-			mov byte ExtraBuf[r11], 0x30
+			mov byte DexBuf[r13], 0x30
+            inc r13
 			jmp .Nothing2
 
-.PrtZ:		mov byte ExtraBuf[r11], 0x31
+.PrtZ:		mov byte DexBuf[r13], 0x31
+            inc r13
 
-.Nothing2:  inc dx
-            inc r11
-            jmp .Next2
+.Nothing2:  jmp .Next2
 
-.Done2:		pop rdx
+.Done2:		call PrintDex
+            pop rdx
             pop rax
 
             jmp SpecOver
@@ -312,37 +404,36 @@ Bspecif:    pop rax
 ;---------------------------------------------------------
 
 Xspecif:    pop rax
+            xor r13, r13
             push rsi
             push rdx
             push rax
 			xor rdx, rdx
 
 .Next1:		call CheckExtraBuf
-            cmp rdx, 4d
+            cmp rax, 0
 			je .Done1
 
 			mov rsi, rax
-			and rsi, 0xF000
-			shr rsi, 12
+			and rsi, 0x000F
 
 			cmp rsi, 0Ah
 			jae .PrtSym
 
 			add rsi, 30h
-			mov ExtraBuf[r11], rsi
-            inc r11
+			mov DexBuf[r13], rsi
+            inc r13
 			jmp .None
 
 .PrtSym:	sub rsi, 0Ah
 			add rsi, 41h
-			mov ExtraBuf[r11], rsi
-            inc r11
+			mov DexBuf[r13], rsi
+            inc r13
 
-.None       inc rdx
-            shl rax, 4
+.None       shr rax, 4
 			jmp .Next1
 
-.Done1:
+.Done1:     call PrintDex
             pop rax
             pop rdx
             pop rsi
@@ -368,5 +459,7 @@ times ('x' - 's' - 1) dq 0
 DexBuf          times 256 db 0
 EXTRABUFLEN     equ 256
 ExtraBuf        times 256 db 0
-Msg:            db "__HELLO %d YOUR MUM", 0x0a, 0x00
+Number          equ 255
+Char            equ 'c'
+Msg:            db "__HELLO %c %d %o %x %s %%%%%%%% %d %c YOUR MUM %s", 0x0a, 0x00
 ASS:            db "YOUR MUM GAY", 0x00
